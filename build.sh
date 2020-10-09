681768ddf24b3150af863e1dc12b60970e9b25bd
akos.pinter <akos.pinter@blabosoft.com>
2020-10-09 22:43:14 +0200

    folder restructuring, local build

---
+++ build.sh
@@ -0,0 +1,13 @@
+BASEDIR=$(dirname $0)
+BUILD_DIRNAME=$BASEDIR/.build_result
+CONTENT_DIR=$BASEDIR/docs
+
+mkdir --mode=777 --parents $BUILD_DIRNAME
+
+# serve
+podman run --rm \
+  --volume="$BUILD_DIRNAME:/srv/jekyll_build" \
+  --volume="$CONTENT_DIR:/srv/jekyll" \
+  -p 8777:8777 \
+  -it jekyll/jekyll:latest \
+  jekyll serve --port 8777 --destination /srv/jekyll_build --disable-disk-cache
